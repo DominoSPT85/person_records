4645e9b816a0cdb2480044d9be8e2a4740692356
Shandra <shandradev85@gmail.com>
2019-02-27 16:01:02 -0700

    added destroy method

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -15,20 +15,38 @@ class PeopleController < ApplicationController
     @person = Person.new(people_params)
 
     if @person.save
-      redirect to people_path
+      redirect_to people_path
     else
       render :new
     end
 
   end
 
+  def edit
+    @person = Person.find(params[:id])
+  end
 
+  def update
+    @person = Person.find(params[:id])
+
+    if @person.update(people_params)
+      redirect_to people_path
+    else
+      render :edit
+    end
+
+  end
+
+  def destroy
+    Person.find(params[:id]).destroy
+    redirect_to people_path
+  end
 
 
   private
 
     def people_params
-      params.require(:page).permit(:name, :age, :gender, :hair_color, :eye_color, :alive)
+      params.require(:person).permit(:name, :age, :gender, :hair_color, :eye_color, :alive)
     end
 
 
