32976d36a07682751543800fc9f3b886cb7bda62
Shandra <shandradev85@gmail.com>
2019-02-27 14:32:30 -0700

    added controller and index, new, show views

--- app/models/person.rb
+++ app/models/person.rb
@@ -8,16 +8,7 @@ class Person < ApplicationRecord
     less_than_or_equal_to: 150
   }
 
-  validates :alive, inclusion: {
-    in: w% ( true, false ),
-    message: "%{value} is invalid"
-  }
-
   validates :name, :age, :alive, :gender, presence: true
-
-  validates :name, uniqueness: true
-
-  validates :hair_color, :eye_color, allow_blank: true
 
 
   # Methods
